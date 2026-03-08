0ea7f2bc3245b330d52572f90ed121200dd9e2d1
kelmcao <kmcao.sh@gmail.com>
2026-03-07 22:24:37 -0800

    changes

--- sketch.js
+++ sketch.js
@@ -1,13 +1,14 @@
 let bg;
 
-function preload() {
-  bg = loadImage('assets/example.jpg');
+function preload(){
+  bg = loadImage("https://fastly.picsum.photos/id/861/1920/1080.jpg?hmac=b2bwytCdkwyzZLP-EqUCh2r2-qkCY-dlkw0UtYyIElM")
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-    background(bg);
+  bg.resize(windowWidth, 0);
+  image(bg, 0, 0);
 }
